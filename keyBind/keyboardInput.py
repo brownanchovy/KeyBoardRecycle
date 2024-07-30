813488be66c676e7a099f1dbd0ae410a127fbf53
brownanchovy <gogodo1022@gmail.com>
2024-07-30 17:40:10 +0900

    setHotkey

--- keyBind/keyboardInput.py
+++ keyBind/keyboardInput.py
@@ -1,75 +1,38 @@
-import keyboard
-import time
-from pynput.keyboard import Listener, Key , KeyCode
-import win32api
-
-def no_keys_pressed():
-    # 모든 알파벳, 숫자, 수정 키에 대해 검사
-    keys_to_check = list(keyboard.all_modifiers) + list(map(str, range(10))) + list('abcdefghijklmnopqrstuvwxyz')
-    # 하나라도 눌려있다면 False 반환
-    for key in keys_to_check:
-        if keyboard.is_pressed(key):
-            return False
-    # 아무 키도 눌려있지 않다면 True 반환
-    return True
-def keyInput():
-    key_input = set()
-    def on_key_event(event):
-        if event.event_type == 'down':  # 키를 눌렀을 때
-            key_input.add(event.name)
-    while True:
-        keyboard.hook(on_key_event)
-        if key_input is True and no_keys_pressed():
-            print("key is selected")
-            return key_input
-
+from pynput.keyboard import Listener, Key, Events
 
 store = set()
+save_store = set()
 
-HOT_KEYS = {
-    'print_hello': set([Key.alt_l, KeyCode(char='1')])
-    , 'open_notepad': set([Key.alt_l, KeyCode(char='2')])
-}
-
-
-def print_hello():
-    print('hello, World!!!')
-
-
-def open_notepad():
-    print('open_notepad')
+def uppertoLower(key):
     try:
-        win32api.WinExec('notepad.exe')
-    except Exception as err:
-        print(err)
-
+        if hasattr(key, 'char') and key.char.isupper():
+            key = key.char.lower()
+    except AttributeError:
+        pass
+    return key
 
 def handleKeyPress(key):
+    key = uppertoLower(key)
     store.add(key)
-
-    for action, trigger in HOT_KEYS.items():
-        CHECK = all([True if triggerKey in store else False for triggerKey in trigger])
-
-        if CHECK:
-            try:
-                action = eval(action)
-                if callable(action):
-                    action()
-            except NameError as err:
-                print(err)
-
+    save_store.add(format(key))
+    print('Press: {}'.format(store))
 
 def handleKeyRelease(key):
+    key = uppertoLower(key)
+    print('Released: {}'.format(key))
     if key in store:
         store.remove(key)
-
-    # 종료
-    if key == Key.esc:
+        print(store)
+        # 종료
+    if key == Key.esc or not store:
         return False
 
-
-with Listener(on_press=handleKeyPress, on_release=handleKeyRelease) as listener:
-    listener.join()
+def setHotkey():
+    with Listener(
+            on_press=handleKeyPress,
+            on_release=handleKeyRelease) as listener:
+        listener.join()
+    print(save_store)
 
 if __name__ == '__main__':
-    handleKeyPress().run()
+    setHotkey().run()
